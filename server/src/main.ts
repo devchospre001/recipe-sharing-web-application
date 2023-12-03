8028392ce59023de9325c09761d8c1a4f8164d1e
Abdul-Kerim Spreco <sprecoabdulkerim@gmail.com>
2023-12-03 22:08:24 +0100

    Added swagger open api documentation to the application

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +11,17 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Recipe Sharing Web Application')
+    .setDescription('Should provide information about user and recipe REST API')
+    .setVersion('1.0')
+    .addTag('recipe-sharing-web-application')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
