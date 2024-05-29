c11eabd7bc8705e56b2d37002f03f72d05e68ed5
Abdul-Kerim Spreco <sprecoabdulkerim@gmail.com>
2024-05-29 16:42:53 +0200

    Server URL removed

--- client/src/api/config.ts
+++ client/src/api/config.ts
@@ -1,11 +1,9 @@
-export const serverUrl = process.env.SERVER_URL;
-
 export const endpoints = {
-  FETCH_ALL_RECIPES: `${serverUrl}/recipes`,
-  FETCH_RECIPES_FROM_USER: `${serverUrl}/recipes/my-recipes`,
-  FETCH_RECIPE_BY_ID: `${serverUrl}/recipes/recipe`,
-  PUBLISH_RECIPE: `${serverUrl}/recipes/recipe/new`,
-  UPDATE_RECIPE: `${serverUrl}/recipes/recipe`, // ID should be provided
-  DELETE_RECIPE: `${serverUrl}/recipes/recipe`, // ID should be provided
-  GET_USER: `${serverUrl}/user/currentLoggedInUser`,
+  FETCH_ALL_RECIPES: `/recipes`,
+  FETCH_RECIPES_FROM_USER: `/recipes/my-recipes`,
+  FETCH_RECIPE_BY_ID: `/recipes/recipe`,
+  PUBLISH_RECIPE: `/recipes/recipe/new`,
+  UPDATE_RECIPE: `/recipes/recipe`, // ID should be provided
+  DELETE_RECIPE: `/recipes/recipe`, // ID should be provided
+  GET_USER: `/user/currentLoggedInUser`,
 };
